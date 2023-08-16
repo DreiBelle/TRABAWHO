ddccd8d1a501235718f42496ba35d9f4088bf6c5
Emman Yu <dontbeshyyu1@gmail.com>
2023-08-16 14:02:26 +0800

    trabawho first commit

---
+++ src/views/Login.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <div>
+        Ion
+    </div>
+</template>
+
+<script setup lang="ts">
+import { IonButton } from '@ionic/vue';
+</script>
+
+<style lang="">
+    
+</style>
